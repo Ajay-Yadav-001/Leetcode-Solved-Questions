86868e20501267a61a4b316faee7a9b9244829ec
AJAY YADAV <ajayyadav15062004@gmail.com>
2026-06-26 11:13:38 +0530

    Time: 13 ms (91.49%), Space: 48.6 MB (90.87%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+
+    public int[][] updateMatrix(int[][] mat) {
+
+        int m = mat.length;
+        int n = mat[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+
+                if(mat[i][j] == 0) {
+                    q.offer(new int[]{i, j});
+                } else {
+                    mat[i][j] = -1;
+                }
+            }
+        }
+
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(!q.isEmpty()) {
+
+            int[] cur = q.poll();
+
+            for(int[] d : dir) {
+
+                int nr = cur[0] + d[0];
+                int nc = cur[1] + d[1];
+
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n && mat[nr][nc] == -1) {
+
+                    mat[nr][nc] = mat[cur[0]][cur[1]] + 1;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
+        }
+
+        return mat;
+    }
+}
