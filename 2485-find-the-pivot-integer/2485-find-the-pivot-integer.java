20aeee27a3b328c0e7d8e5e26c927158898542cd
Ajay Yadav <ajayyadav15062004@gmail.com>
2026-09-23 15:59:39 +0530

    Time: 0 ms (100%), Space: 42.2 MB (85.25%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -3,6 +3,10 @@ class Solution {
         int totalSum = n * (n + 1)/2;
         int x = (int) Math.sqrt(totalSum);
 
-        return (x * x == totalSum) ? x : -1; 
+        if (x * x == totalSum) {
+            return x;
+        } else {
+            return -1;
+        }
     }
 }
