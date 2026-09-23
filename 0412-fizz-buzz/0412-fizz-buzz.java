1e0a6341b342f0ca9901aaca59584a6de3130db0
Ajay Yadav <ajayyadav15062004@gmail.com>
2026-09-23 16:33:05 +0530

    Time: 1 ms (99.82%), Space: 46.5 MB (95.78%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -3,27 +3,17 @@ import java.util.List;
 
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> newList = new ArrayList<>(n);
-        
-        int fizz = 0;
-        int buzz = 0;
+        List<String> newList = new ArrayList<>();
         
         for (int i = 1; i <= n; i++) {
-            fizz++;
-            buzz++;
-            
-            if (fizz == 3 && buzz == 5) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 newList.add("FizzBuzz");
-                fizz = 0;
-                buzz = 0;
-            } else if (fizz == 3) {
+            } else if (i % 3 == 0) {
                 newList.add("Fizz");
-                fizz = 0;
-            } else if (buzz == 5) {
+            } else if (i % 5 == 0) {
                 newList.add("Buzz");
-                buzz = 0;
             } else {
-                newList.add(String.valueOf(i));
+                newList.add(Integer.toString(i)); 
             }
         }
         
